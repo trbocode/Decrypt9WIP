9f903364433575fdefc8c5779c52e096b4861ba5
d0k3 <tore.anon@gmail.com>
2015-07-11 15:29:33 +0200

    Fix system title dumper workdir processing

--- source/debugfs.h
+++ source/debugfs.h
@@ -13,6 +13,3 @@ bool DebugFileRead(void* buf, size_t size, size_t foffset);
 
 /** Writes to the opened file */
 bool DebugFileWrite(void* buf, size_t size, size_t foffset);
-
-/** Creates a directory */
-bool DebugDirMake(const char* path);
