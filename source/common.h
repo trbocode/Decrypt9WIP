ad9a1c7c610360511857cf3b1e299b0bea6d327f
Shadowtrance <teronskyn@gmail.com>
2015-07-12 12:53:57 +1000

    Moved that #define to common.h

--- source/common.h
+++ source/common.h
@@ -25,7 +25,17 @@
     ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
        _a < _b ? _a : _b; })
-
+#define le16(a) \
+    (((u8*) (a))[0] | (((u8*) (a))[1]<<8))
+#define le32(a) \
+    (((u8*) (a))[0] | (((u8*) (a))[1]<<8) | \
+    (((u8*) (a))[2]<<16) | (((u8*) (a))[3]<<24))
+#define be16(a) \
+    (((u8*) (a))[1] | (((u8*) (a))[0]<<8))
+#define be32(a) \
+    (((u8*) (a))[3] | (((u8*) (a))[2]<<8) | \
+    (((u8*) (a))[1]<<16) | (((u8*) (a))[0]<<24))
+    
 inline char* strupper(const char* str) {
     char* buffer = (char*)malloc(strlen(str) + 1);
     for (int i = 0; i < strlen(str); ++i)
