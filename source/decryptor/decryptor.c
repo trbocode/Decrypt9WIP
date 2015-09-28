de8cb6652729d11e98ed282107d4bd94be7bef87
d0k3 <tore.anon@gmail.com>
2015-09-28 17:24:45 +0200

    Added fragmentation check for SeekFileInNand
    ... meaning extracting files > 32kB is safer now!

--- source/decryptor/decryptor.c
+++ source/decryptor/decryptor.c
@@ -637,13 +637,10 @@ u32 SeekFileInNand(u32* offset, u32* size, const char* path, PartitionInfo* part
     // - dirs must not exceed 1024 entries
     // - fragmentation not supported
     
-    const static char zeroes[8+3] = { 0x00 };
     u8* buffer = BUFFER_ADDRESS;
     u32 p_size = partition->size;
     u32 p_offset = partition->offset;
-    
-    u32 cluster_size;
-    u32 cluster_start;
+    u32 fat_pos = 0;
     bool found = false;
     
     if (strnlen(path, 256) % (8+3) != 0)
@@ -653,17 +650,19 @@ u32 SeekFileInNand(u32* offset, u32* size, const char* path, PartitionInfo* part
     
     // good FAT header description found here: http://www.compuphase.com/mbr_fat.htm
     u32 fat_start = NAND_SECTOR_SIZE * (*((u16*) (buffer + 0x0E)));
-    u32 fat_size = NAND_SECTOR_SIZE * (*((u16*) (buffer + 0x16)) * buffer[0x10]);
+    u32 fat_count = buffer[0x10];
+    u32 fat_size = NAND_SECTOR_SIZE * (*((u16*) (buffer + 0x16)) * fat_count);
     u32 root_size = *((u16*) (buffer + 0x11)) * 0x20;
-    cluster_start = fat_start + fat_size + root_size;
-    cluster_size = buffer[0x0D] * NAND_SECTOR_SIZE;
+    u32 cluster_start = fat_start + fat_size + root_size;
+    u32 cluster_size = buffer[0x0D] * NAND_SECTOR_SIZE;
     
     for (*offset = p_offset + fat_start + fat_size; strnlen(path, 256) >= 8+3; path += 8+3) {
         if (*offset - p_offset > p_size)
             return 1;
         found = false;
         DecryptNandToMem(buffer, *offset, cluster_size, partition);
-        for (u32 i = 0x00; i < cluster_size; i += 0x20) {
+        for (u32 i = 0x00; i < cluster_size; i += 0x20) {            
+            const static char zeroes[8+3] = { 0x00 };
             // skip invisible, deleted and lfn entries
             if ((buffer[i] == '.') || (buffer[i] == 0xE5) || (buffer[i+0x0B] == 0x0F))
                 continue;
@@ -673,12 +672,24 @@ u32 SeekFileInNand(u32* offset, u32* size, const char* path, PartitionInfo* part
             for (p = 0; (p < 8+3) && (path[p] == '?' || buffer[i+p] == path[p]); p++);
             if (p != 8+3) continue;
             // entry found, store offset and move on
-            *offset = p_offset + cluster_start + (*((u16*) (buffer + i + 0x1A)) - 2) * cluster_size;
+            fat_pos = *((u16*) (buffer + i + 0x1A));
+            *offset = p_offset + cluster_start + (fat_pos - 2) * cluster_size;
             *size = *((u32*) (buffer + i + 0x1C));
             found = true;
             break;
         }
         if (!found) break;
+    }
+    
+    // check for fragmentation
+    if (found && (*size > cluster_size)) {  
+        if (fat_size / fat_count > 0x100000) // prevent buffer overflow
+            return 1; // fishy FAT table size - should never happen
+        DecryptNandToMem(buffer, p_offset + fat_start, fat_size / fat_count, partition);
+        for (u32 i = 0; i < (*size - 1) / cluster_size; i++) {
+            if (*((u16*) buffer + fat_pos + i) != fat_pos + i + 1)
+                return 1;
+        } // no need to check the files last FAT table entry
     }
     
     return (found) ? 0 : 1;
