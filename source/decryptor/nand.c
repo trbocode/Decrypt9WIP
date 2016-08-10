95e46298abe021b88c64d123a8d0c6702a60002b
d0k3 <tore.anon@gmail.com>
2016-08-10 21:07:31 +0200

    More relaxed checks for injectable files

--- source/decryptor/nand.c
+++ source/decryptor/nand.c
@@ -823,7 +823,9 @@ u32 EncryptFileToNand(const char* filename, u32 offset, u32 size, PartitionInfo*
     if (!DebugFileOpen(filename))
         return 1;
     
-    if (FileGetSize() != size) {
+    u32 fsize = FileGetSize();
+        size = fsize;
+    } else if (fsize != size) {
         Debug("%s has wrong size", filename);
         FileClose();
         return 1;
